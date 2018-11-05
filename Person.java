ffbb41b3cfbfe76370aa0b8a9071e7301e6e68a1
DmitryLipiev <dmitry.lipiyov@gmail.com>
2018-11-05 16:50:27 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,36 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.envers.Audited;
+
+/**
+ * Person information.
+ */
+@Entity
+@Table(name = "persons")
+@Audited
+@Data
+@EqualsAndHashCode(exclude = "id")
+public class Person {
+
+    @Id
+    @Column(name = "person_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", length = 45)
+    private String lastName;
+
+    @Column(name = "middle_name", length = 45)
+    private String middleName;
+
+}
