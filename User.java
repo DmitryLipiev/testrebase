0c984a7b803a6cbe614642ec1665b0a79e6b7ae4
Dmitry Lipiev <DmitryLipiev@coherentsolutions.com>
2018-11-06 15:24:13 +0300

    made field non-nullable

--- User.java
+++ User.java
@@ -38,4 +38,77 @@ public class User implements Serializable {
     @Column(name = "user_id")
     private Long id;
 
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, optional = true)
+    @MapsId
+    @JoinColumn(name = "user_id", nullable = false)
+    private Person person;
+
+    @Column(name = "user_role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(name = "attempts", nullable = false)
+    private Long attempts = 0L;
+
+    @ToString.Exclude
+    @Column(name = "pwd",)
+    @NotAudited
+    private byte[] password;
+
+    @Column(name = "pwd_expired")
+    @NotAudited
+    private LocalDate passwordExpirationDate;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "alt_email")
+    private String alternativeEmail;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user")
+    @NotAudited
+    private Set<SecurityAnswer> securityAnswers;
+
+    @Column(name = "user_status")
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column(name = "last_login_datetime")
+    @NotAudited
+    private LocalDateTime lastLoginDateTime;
+
+    @Column(name = "last_reg_email")
+    @NotAudited
+    private LocalDateTime lastRegistrationEmail;
+
+    @Column(name = "real_user")
+    @NotAudited
+    private Boolean realUser;
+
+    @Transient
+    public boolean isHistoric() {
+        return this.getStatus() == UserStatus.HISTORIC;
+    }
+
+    @Transient
+    public boolean isPasswordExpired() {
+        return LocalDate.now().isAfter(passwordExpirationDate);
+    }
+
+    @Transient
+    public boolean isActive() {
+        return this.getStatus() == UserStatus.ACTIVE;
+    }
+
+    @Transient
+    public boolean isAdmin() {
+        return this.getRole() == UserRole.ADMINISTRATOR;
+    }
+
+    @Transient
+    public boolean isApiUser() {
+        return this.getRole() == UserRole.API_USER;
+    }
+
 }
